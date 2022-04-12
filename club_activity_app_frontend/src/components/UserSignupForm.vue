ccbaec5972566966352438f2323087bf9dbea01f
koki-mus <lkxl1239@yahoo.co.jp>
2022-04-12 11:03:21 +0900

    signin form, update, submit button can be disable

--- club_activity_app_frontend/src/components/UserSignupForm.vue
+++ club_activity_app_frontend/src/components/UserSignupForm.vue
@@ -3,8 +3,8 @@
     <h1>Sigin up</h1>
       <form v-on:submit.prevent="doSignup">
         <label>ユーザーID</label><br>
-        <input type="text" placeholder="user id" v-model="user.userId" /><br>
-
+        <input type="text" placeholder="user id" v-model="user.userId" v-on:input="make_userId_message"/><br>
+        <span v-html="userId_message"></span>
         <label>パスワード</label><br>
         <input type="password" placeholder="password" v-model="pass1" v-on:input="make_pass1_message(); make_pass2_message()"/><br>
         <span v-html="pass1_message"></span>
@@ -13,7 +13,7 @@
         <input type="password" placeholder="password" v-model="pass2" v-on:input="make_pass2_message"/><br>
         <span v-html="pass2_message"></span>
 
-        <button type="submit" :disabled="!pass1_flag || !pass2_flag" >Sign In</button>
+        <button type="submit" :disabled="!pass1_flag || !pass2_flag || !userId_flag" >Sign In</button>
 
       </form>
       <!-- <p class="resMessage" v-html="resMessage"></p> -->
@@ -32,8 +32,10 @@ export default {
         pass2:"",
         pass1_message:"<br>",
         pass2_message:"<br>",
+        userId_message:"<br>",
         pass1_flag: false,
         pass2_flag: false,
+        userId_flag: false,
         // resMessage:"<p></p>"
     };
   },
@@ -74,22 +76,39 @@ export default {
       var message = ""
       if(this.pass1_flag){
         if (this.pass1 == this.pass2){
-          //ok
           message = "<font color='green'>パスワードが一致しました。</font><br>"
           this.pass2_flag = true
-        }
-        else{
-          //一致しない
+        }else{
           this.pass2_flag = false
           message = "<font color='red'>パスワードが一致しません。</font><br>"
         }
-      }
-      else{
-        //pass1に問題あり
+      }else{
         this.pass2_flag = false
       }
       this.pass2_message = message
-    }
+    },
+    make_userId_message() {
+      var message = ""
+      if (this.user.userId.length == 0) {
+        message += "入力は必須です。"
+      }else{
+        if(this.user.userId.length < 8) {
+          message += "8文字以上にしてください。"
+        }
+      }
+      if (this.user.userId.length >= 32) {
+        message += "32文字以下にしてください。"
+      }
+      if (/\W+/g.test(this.user.userId)){
+        message += "半角英数字と'_'のみ使えます。"
+      }
+      if (message == "") {
+        this.userId_flag = true
+      } else {
+        this.userId_flag = false
+      }
+      this.userId_message = "<font color='red'>"+message+"</font><br>"
+    },
   }
 }
 </script>
